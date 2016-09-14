8d2054cafc358e013e3e712452facc542ca8ef60
infojunkie <karim.ratib@gmail.com>
2016-09-14 13:41:19 -0700

    Add  to collect config files

---
+++ tar-config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tar zcf config.tar.gz check-api/config/config.yml check-api/config/database.yml pender/config/config.yml pender/config/database.yml check-web/config.js check-web/test/config.js check-web/test/config.yml
