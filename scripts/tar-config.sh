0c7591e8d23cdf2c6b3819117fa51610176501b3
infojunkie <karim.ratib@gmail.com>
2016-09-29 11:44:36 -0700

    Update submodules and config archiving script

--- scripts/tar-config.sh
+++ scripts/tar-config.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-tar zcf config.tar.gz check-api/config/config.yml check-api/config/database.yml pender/config/config.yml pender/config/database.yml check-web/config.js check-web/test/config.js check-web/test/config.yml
+filename=config-`date +%Y%m%d%H%M`.tar.gz
+tar zcf $filename check-api/config/config.yml check-api/config/database.yml pender/config/config.yml pender/config/database.yml check-web/config.js check-web/test/config.js check-web/test/config.yml
+echo Created file $filename
