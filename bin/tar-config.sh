5da94564d56ee1a3f9b2e91f4ebdab34ae15ce96
Caio SBA <caiosba@gmail.com>
2017-01-17 17:41:19 -0300

    Ticket #5614: Adding sidekiq.yml to bin/tar-config.sh

--- bin/tar-config.sh
+++ bin/tar-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 filename=config-`date +%Y%m%d%H%M`.tar.gz
-tar zcf $filename check-api/config/config.yml check-api/config/database.yml pender/config/config.yml pender/config/database.yml check-web/config.js check-web/test/config.js check-web/test/config.yml
+tar zcf $filename check-api/config/config.yml check-api/config/database.yml check-api/config/sidekiq.yml pender/config/config.yml pender/config/database.yml check-web/config.js check-web/test/config.js check-web/test/config.yml
 echo Created file $filename
