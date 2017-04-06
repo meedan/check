a69fad09198e51efc78486b4bf22e3a96d32c9aa
infojunkie <karim.ratib@gmail.com>
2017-04-06 15:34:07 -0700

    Update submodules

--- bin/docker-clean.sh
+++ bin/docker-clean.sh
@@ -5,4 +5,6 @@ docker rm -v $(docker ps -a -q -f status=exited)
 
 docker rmi $(docker images -f "dangling=true" -q)
 
+docker volume prune -f
+
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
