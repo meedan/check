1c41ee866bd20fe061468d9abe599c1e33c8dad2
infojunkie <karim.ratib@gmail.com>
2017-05-27 09:27:33 -0700

    Better git-update

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 # http://stackoverflow.com/q/1777854/209184
-git submodule foreach -q --recursive 'echo Updating $name && git remote prune origin && git pull && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+git pull && git submodule foreach -q --recursive 'echo Updating $name && git remote prune origin && git pull && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
