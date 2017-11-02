7c1bfcbd686bad3aa6f3186cc861d3c0e25e1585
infojunkie <karim.ratib@gmail.com>
2017-11-02 15:56:59 -0700

    Update script now updates configs too

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -e
 # http://stackoverflow.com/q/1777854/209184
-git remote prune origin && git pull && git submodule foreach -q --recursive 'echo Updating $name && git remote prune origin && git pull && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+PRODUCT=$(grep 'remote "origin"' -A 1 .git/config | tail -1 | cut -d'/' -f 2 | cut -d'.' -f1 )
+echo Updating $PRODUCT && git remote prune origin && git pull
+git submodule foreach -q --recursive 'echo Updating $name && git remote prune origin && git pull && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+if [ -d "configurator" ]; then
+  echo Updating configuration
+  configurator/do.sh deploy local
+fi
