a037f138e7af6e2dcce26a97d60fceaa6d768b84
infojunkie <karim.ratib@gmail.com>
2017-11-02 17:27:47 -0700

    Update script also pulls latest configs

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -5,5 +5,6 @@ echo Updating $PRODUCT && git remote prune origin && git pull
 git submodule foreach -q --recursive 'echo Updating $name && git remote prune origin && git pull && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
 if [ -d "configurator" ]; then
   echo Updating configuration
+  cd configurator && git pull && cd ..
   configurator/do.sh deploy local
 fi
