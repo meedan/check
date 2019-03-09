ef3559336c11fea9277131858ce361c481724e83
infojunkie <karim.ratib@gmail.com>
2019-03-09 11:23:11 -0800

    git-update never attempts to squash when pulling

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -2,7 +2,7 @@
 # http://stackoverflow.com/q/1777854/209184
 PRODUCT=$(grep 'remote "origin"' -A 1 .git/config | tail -1 | cut -d'/' -f 2 | cut -d'.' -f1 )
 echo Updating $PRODUCT && git remote prune origin && git pull
-git submodule foreach -q --recursive 'echo Updating $name && git remote prune origin && git pull && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+git submodule foreach -q --recursive 'echo Updating $name && git remote prune origin && git pull --no-squash && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
 if [ -d "configurator" ]; then
   echo Updating configuration
   cd configurator && git pull && cd ..
