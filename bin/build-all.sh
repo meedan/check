bce7b7e32d8542b6496aaeff0e28ec06ce0daaba
infojunkie <karim.ratib@gmail.com>
2020-03-28 15:23:29 -0700

    bash not sh

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash
 git submodule foreach bash -c 'git checkout develop'
 find -name '*.example' -not -path '*apollo*' | while read f; do cp "$f" "${f%%.example}"; done
 docker system prune --all --volumes
