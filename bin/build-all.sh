e32d39dddb68983dd2f444a2cfc02d6a855960e2
infojunkie <karim.ratib@gmail.com>
2020-03-30 13:03:25 -0700

    BSD friendly

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git submodule foreach bash -c 'git checkout develop'
-find -name '*.example' -not -path '*apollo*' | while read f; do cp "$f" "${f%%.example}"; done
+find . -name '*.example' -not -path '*apollo*' | while read f; do cp "$f" "${f%%.example}"; done
 docker system prune --all --volumes
 docker-compose build
 docker-compose up --abort-on-container-exit
