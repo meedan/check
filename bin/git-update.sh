4d3329095cbf79aab02133142407ab7d634a8f40
infojunkie <karim.ratib@gmail.com>
2020-04-29 14:37:12 -0700

    git submodule update --init messes things up

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -2,7 +2,6 @@
 # http://stackoverflow.com/q/1777854/209184
 APP=$(grep 'remote "origin"' -A 1 .git/config | tail -1 | cut -d'/' -f 2 | cut -d'.' -f1 )
 echo Updating $APP && git remote prune origin && git pull --no-squash
-git submodule update --init
 git submodule foreach -q --recursive 'echo Updating $name; git remote prune origin; branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch || echo develop); current=$(git rev-parse --abbrev-ref HEAD); if [ "$current" = "$branch" ]; then git pull --no-squash; else git fetch origin $branch:$branch; fi'
 if [ -d "configurator" ]; then
   echo Updating configuration
