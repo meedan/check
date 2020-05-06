f4917069030a1d72bed47d569b850c570e66cba2
infojunkie <karim.ratib@gmail.com>
2020-05-06 12:21:08 -0700

    Don't nuke volumes

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git submodule foreach bash -c 'git checkout develop'
 find . -name '*.example' -not -path '*apollo*' | while read f; do cp "$f" "${f%%.example}"; done
-docker system prune --all --volumes
 docker-compose build
 docker-compose up --abort-on-container-exit
