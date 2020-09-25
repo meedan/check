75202be5080a370bb61c1c77b6e12c860e212c3a
Daniela Soares Feitosa <daniela@meedan.com>
2020-09-25 17:30:59 -0300

    Display how to use the script if user sends the wrong number of args

--- bin/release.sh
+++ bin/release.sh
@@ -6,26 +6,45 @@
 
 # The user needs to push to repository
 
-version=$1
-echo -e "\n============= Releasing version: $version ===============\n"
+usage() {
+  echo -e "Usage:\n  $0 <version>\n"
+  echo -e "This script releases a new version for Check:"
+  echo "  Update all submodules to tag <version>"
+  echo "  Create a commit with the updated submodules"
+  echo "  Create a tag <version>"
+  echo "  Ask the user to push the commit and tag"
+}
 
-git submodule foreach -q "
-  echo Checking version $version for submodule \$name.;
-  if git rev-parse "$version" >/dev/null 2>&1; then
-    echo \$name will be updated.
-    git checkout -q $version
-  else
-    echo \$name will not be updated. Tag \$version does not exist.
-  fi
-  echo
-"
+release() {
+  echo -e "\n============= Releasing version: $version ===============\n"
 
-git add -u
-git commit -m "Updated submodules to $version"
-git tag $version
-git show
+  git submodule foreach -q "
+    echo Checking version $version for submodule \$name.;
+    if git rev-parse "$version" >/dev/null 2>&1; then
+      echo \$name will be updated.
+      git checkout -q $version
+    else
+      echo \$name will not be updated. Tag \$version does not exist.
+    fi
+    echo
+  "
 
-echo -e "\n***** Release $version is ready *****\n"
-echo "I: please verify the commit and push the code and tag to remote"
-echo "I: $ git push origin master"
-echo "I: $ git push origin $version"
+  git add -u
+  git commit -m "Updated submodules to $version"
+  git tag $version
+  git show
+
+  echo -e "\n***** Release $version is ready *****\n"
+  echo "I: please verify the commit and push the code and tag to remote"
+  echo "I: $ git push origin master"
+  echo "I: $ git push origin $version"
+}
+
+version="$1"
+if [ -z "$version" ] || [ $# -gt 1 ]; then
+  echo -e "\e[41mError:\e[0m wrong number of arguments (given $#, expected 1)\n"
+  usage
+  exit 1
+fi
+
+release
