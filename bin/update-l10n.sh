4bffea446f139713244a5dcf2d1b11c0c2a92773
Caio Almeida <caiosba@gmail.com>
2021-10-05 14:46:05 -0300

    Updated submodules to v0.135.0

--- bin/update-l10n.sh
+++ bin/update-l10n.sh
@@ -12,17 +12,17 @@ echo 'Updating Check Web translations...' && \
 echo 'Updating Check Mark translations...' && \
 (docker-compose exec mark npm run transifex:upload) && \
 (docker-compose exec mark npm run transifex:download) && \
-echo 'Updating Check Search translations...' && \
-(docker-compose exec search npm run transifex:upload) && \
-(docker-compose exec search npm run transifex:download) && \
+# echo 'Updating Check Search translations...' && \
+# (docker-compose exec search npm run transifex:upload) && \
+# (docker-compose exec search npm run transifex:download) && \
 echo 'Done! Please check the changes below:' && \
 echo 'Check API:' && cd check-api && git status && cd - && \
 echo 'Check Web:' && cd check-web && git status && cd - && \
 echo 'Check Mark:' && cd check-mark && git status && cd - && \
-echo 'Check Search:' && cd check-search && git status && cd - && \
+# echo 'Check Search:' && cd check-search && git status && cd - && \
 # It's safer to run the ones below manually:
 echo 'Should be all updated - if it looks good to you, please push to `develop`:' && \
 echo "cd check-api && git commit config/locales -m 'CHECK-109: Updating l10n' && git push && cd -" && \
 echo "cd check-web && git commit localization -m 'CHECK-109: Updating l10n' && git push && cd -" && \
-echo "cd check-mark && git commit src/localization -m 'CHECK-109: Updating l10n' && git push && cd -" && \
-echo "cd check-search && git commit localization -m 'CHECK-109: Updating l10n' && git push && cd -"
+echo "cd check-mark && git commit src/localization -m 'CHECK-109: Updating l10n' && git push && cd -"
+# echo "cd check-search && git commit localization -m 'CHECK-109: Updating l10n' && git push && cd -"
