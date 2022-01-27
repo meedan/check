67497f0b96db7ebdf8c8a3b49f8b73a2986d2e31
danielevalverde <danierzevalverde@gmail.com>
2022-01-27 17:37:39 -0300

    debug

--- bin/first-build.sh
+++ bin/first-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git submodule foreach bash -c 'git checkout develop'
+git submodule foreach bash -c 'git checkout master'
 find . -name '*.example' -not -path '*apollo*' | while read f; do cp "$f" "${f%%.example}"; done
 docker-compose build
 docker-compose up --abort-on-container-exit
