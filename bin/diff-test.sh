785c394f850acb77cd65f5ca16bf4bb7ed7eb7de
manu vasconcelos <manu@meedan.com>
2023-06-06 08:58:24 -0300

    make subdirectorys for services with multiple configs
    currently it's only saving the last config from check-bots, and running
    all of them against the last one

--- bin/diff-test.sh
+++ bin/diff-test.sh
@@ -1,74 +1,58 @@
 #!/bin/bash
 
 # rm -r ./temp
-mkdir ./temp
 
-git submodule foreach -q '
-  configs=(`find . -name 'config.*.example'`)
+# git submodule foreach -q --recursive '
+#   configs=(`find . -name 'config.*.example'`)
 
-  if [[ $configs ]]; then
-    for config in "${configs[@]}"; do 
-      echo $name: ${config}
-      mkdir ../temp/$name
-      cp -r ${config} ../temp/$name
-    done 
-  fi
-'
+#   if [[ $configs ]]; then
+#     for config in "${configs[@]}"; do 
+#       mkdir -p ../temp/$name/${config%/*}
+#       cp -r ${config} ../temp/$name/${config%/*}
+#     done 
+#   fi
+
+#   echo Updating $name
+#   git remote prune origin
+#   branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch || echo develop)
+#   current=$(git rev-parse --abbrev-ref HEAD)
+#   if [ "$current" = "$branch" ]; then
+#     git pull --no-squash
+#   else
+#     git fetch origin $branch:$branch
+#   fi
+#   if [ -e $toplevel/$name/.githooks ]; then
+#     hooks=$(git rev-parse --git-dir)/hooks
+#     find $hooks -type l -exec rm \{\} \;
+#     find $toplevel/$name/.githooks -type f -exec ln -sf \{\} $hooks \;
+#   fi
+# '
 
 git submodule foreach -q --recursive '
-  echo Updating $name
-  git remote prune origin
-  branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch || echo develop)
-  current=$(git rev-parse --abbrev-ref HEAD)
-  if [ "$current" = "$branch" ]; then
-    git pull --no-squash
-  else
-    git fetch origin $branch:$branch
-  fi
-  if [ -e $toplevel/$name/.githooks ]; then
-    hooks=$(git rev-parse --git-dir)/hooks
-    find $hooks -type l -exec rm \{\} \;
-    find $toplevel/$name/.githooks -type f -exec ln -sf \{\} $hooks \;
-  fi
-'
-
-git submodule foreach -q '
   configs=(`find . -name 'config.*.example'`)
 
-  for config in "${configs[@]}"; do 
-    changes=`diff -u ../temp/$name/${config##*/} ./${config#*/}`
+  RED="\033[0;31m"
+  NC="\033[0m"
 
-    echo —————————————————
-    echo $name
+  for config in "${configs[@]}"; do 
+    changes=`diff -u ../temp/$name/${config%/*}/${config##*/} ./${config#*/}`
+
+    printf '—%.0s' {1..50} 
+    printf "\n"
 
     if [[ $changes ]]; then
-      diff -u ../temp/$name/${config##*/} ./${config#*/}
+      echo ${RED}$name: has changes, consider updating your config file${NC}
+
+      echo ../temp/$name/${config%/*}/${config##*/} 
+      echo ./${config#*/}
+
+      diff -u ../temp/$name/${config%/*}/${config##*/} ./${config#*/}
     else
-      echo no changes
+      echo $name: no changes
     fi
-  done 
+  done
 '
 
+# diff /Users/manu/work/check/temp/pender/config/config.yml.example /Users/manu/work/check/pender/config/config.yml.example
+# diff ./temp/check-bots/./published-reports/config.js.example ./check-bots/published-reports/config.js.example
 # rm -r ./temp
-
-  # changes=`git diff-index origin/develop -- $configs`
-
-  # RED="\033[0;31m"
-  # NC="\033[0m"
-
-  # echo —
-  # echo submodule: $name 
-  # if [[ $configs ]]; then
-  #   for config in "${configs[@]}"; do  
-  #   done 
-  # else
-  #   echo config file: none 
-  # fi
-
-  # if [[ $changes ]]; then
-  #   echo ${RED}">>>>" $name has a changed config${NC}
-
-  #   for config in "${configs[@]}"; do  
-  #     git diff origin/develop -- $config
-  #   done     
-  # fi
