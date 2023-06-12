2fdd7daae0b53264dd635ced3557481d1939e5ef
manu vasconcelos <manu@meedan.com>
2023-06-12 10:24:25 -0300

    update to  use variable instead of fucntion
    when I was first testing things I used a funtion to build the path and
    get it again later, but a variable is enough

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -e
-CONFIGS=($(find . -name '*.example' -not -path '*/temp_config_files_2/*'))
-RED="\033[0;31m"
-NC="\033[0m"
+configs=($(find . -name '*.example' -not -path '*/temp/*'))
+dest="./temp"
 
-dest () {
-  echo "./temp_config_files_2/${1%/*}" 
-}
+red="\033[0;31m"
+nc="\033[0m"
 
-for CONFIG in "${CONFIGS[@]}"; do
-  mkdir -p "$(dest $CONFIG)"
-  cp -r "$CONFIG" "$(dest  $CONFIG)"
+for config in "${configs[@]}"; do
+  mkdir -p "${dest}/${config}"
+  cp -r "$config"  "${dest}/${config}"
 done
 
 echo Updating check
@@ -33,30 +31,31 @@ git submodule foreach -q --recursive '
   fi
 '
 
-if [ -d configurator ]; then
-  echo Updating configuration
-  (cd configurator && git pull --no-squash)
-  configurator/do.sh deploy local
-fi
-docker-compose -f docker-compose.yml -f docker-test.yml build
+# if [ -d configurator ]; then
+#   echo Updating configuration
+#   (cd configurator && git pull --no-squash)
+#   configurator/do.sh deploy local
+# fi
+# docker-compose -f docker-compose.yml -f docker-test.yml build
 
 echo —
 echo Checking for updated config files
 set +e
-for CONFIG in "${CONFIGS[@]}"; do
-  changes=$(diff -u "$(dest $CONFIG)/${CONFIG##*/}" "$CONFIG")
+for config in "${configs[@]}"; do
+  changes=$(diff -u "${dest}/${config}" "$config")
 
   if [[ "$changes" ]]; then
     has_changes="true"
     echo —
-    echo -e "$RED $CONFIG has changes."
-    echo -e "Consider updating your config file. $NC"
+    echo -e "$red $config has changes."
+    echo -e "Consider updating your config file. $nc"
     echo "$changes"
   fi  
 done
+set -e
 
 if [ "$has_changes" != "true" ]; then
   echo Config files have no changes.
 fi
 
-rm -r ./temp_config_files_2
+rm -r ./temp
