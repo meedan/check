1cdd82c2b11c64728c33f504d2459d3d91fb090f
Chinelo Obitube <74656858+chinelo-obitube@users.noreply.github.com>
2024-11-14 16:08:14 +0100

    Cv2-5632 migrate check ci (#99)
    migrate check ci 
    
    ---------

--- bin/first-build.sh
+++ bin/first-build.sh
@@ -18,5 +18,5 @@ replace_secret 'check-api' 'config/config.yml' 'google_client_id'
 replace_secret 'check-api' 'config/config.yml' 'google_client_secret'
 
 # Build & Run
-docker-compose build
-docker-compose up --abort-on-container-exit
+docker compose build
+docker compose up --abort-on-container-exit
