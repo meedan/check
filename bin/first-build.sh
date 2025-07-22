e749b64b1b55acee9fed01a79a2a30c44d8893a7
danielevalverde <danierzevalverde@gmail.com>
2025-07-22 12:56:06 -0300

    try to fix problem with presto

--- bin/first-build.sh
+++ bin/first-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Go to the develop branch of each repository
-git submodule foreach bash -c '(git checkout develop || git checkout master || git checkout main) && git pull'
+git submodule foreach bash -c ' git clean -fd (git checkout develop || git checkout master || git checkout main) && git pull'
 
 # Copy the example files
 find . -name '*.example' -not -path '*apollo*' | while read f; do cp "$f" "${f%%.example}"; done
@@ -20,6 +20,7 @@ replace_secret 'check-api' 'config/config.yml' 'google_client_secret'
 # echo "Check Mark Branch and Dockerfile:"
 # cd check-mark && git status && git pull origin develop && cd ..
 cat check-mark/Dockerfile
+echo "Presto Requirements:"
 cat presto/requirements.txt
 # Build & Run
 docker compose build --pull --no-cache
