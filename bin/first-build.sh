f612b86b5502754c9fcc422c05fc81d18307a06d
danielevalverde <danierzevalverde@gmail.com>
2025-07-24 15:00:37 -0300

    debug alegre

--- bin/first-build.sh
+++ bin/first-build.sh
@@ -30,7 +30,7 @@ echo "Building Presto images..."
 docker compose build presto-server
 echo "Building Alegre image..."
 docker compose build alegre
-echo "Building Queue Worker image..."
-docker compose build queue_worker
+# echo "Building Queue Worker image..."
+# docker compose build queue_worker
 #docker compose build
 # docker compose up --abort-on-container-exit
